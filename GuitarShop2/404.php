25b19e1498ddae8241854d179ce885fa232064ab
juurailmjarv <juura.ilmjarv@gmail.com>
2018-10-26 11:13:30 +0100

    some changes to shopping cart

---
+++ GuitarShop2/404.php
@@ -0,0 +1,217 @@
+<?php
+?>
+<html>
+<h1>ERROR</h1>
+<div class="block">
+	  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item-o"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+  <div class="item"></div>
+
+</div>
+  <h2>OOPS! Could not find the page!</h2>
+  <p>hover <u>all</u> the block to discover <br />the number of the error</p>
+</html>
+<style>
+html {font-size:16px; background:#EC87C0;}
+h1, h2, p {
+font-family:arial, sans-serif;
+text-align:center;
+color:#D770AD;
+width:320px;
+margin:auto;
+}
+h1 {font-size:5.5em;}
+h2 {font-size:2em;word-spacing:0.25em;}
+p {font-size:1em;color:#EC87C0;background:#D770AD;padding:5px;}
+
+u {
+color:#DA4453;
+font-weight:800;
+}
+
+.block {
+
+width:320px;
+height:160px;
+background:#EC87C0;
+border:15px solid #D770AD;
+border-radius:3px;
+margin:auto;
+cursor:pointer;
+}
+.item, .item-o {
+display:inline-block;
+margin:-2px;
+vertical-align:top;
+width:16px;
+height:16px;
+}
+.item {
+background:#EC87C0;
+}
+.item-o {
+background:#EC87C0;
+transition:all 23s;
+}
+.item-o:hover {
+transition:all 0.0000001s;
+background:#D770AD;
+}
+</style>
--- GuitarShop2/config.php
+++ GuitarShop2/config.php
@@ -18,7 +18,9 @@ $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
 if($db === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect to database!" . mysqli_connect_error());
+
+
 }
 
 
