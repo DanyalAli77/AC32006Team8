26fef9377f6f3d06f86bdbd1a3a65d8ccfda4dee
Matt Foulis <MatthewFoulis1@gmail.com>
2018-10-30 11:40:56 +0000

    Basic about page

--- GuitarShop2/about.php
+++ GuitarShop2/about.php
@@ -1,7 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Juura
- * Date: 23/10/2018
- * Time: 18:44
- */
+session_start();
+
+require_once('config.php');
+if(isset($_SESSION['loggedin']))
+{
+    include('header_inside.php');
+}
+else{
+    include ('header.php');
+}
+if(!empty($_SESSION['cart'])) {
+    $items =  $_SESSION['cart'];
+}
+
+?>
+
+    <div class="container">
+        <p>
+            <h1>About page</h1>
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+
+
+
+<?php
+include ('footer.php');
+?>
