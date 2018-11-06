e6bbb166371b1f172cad85d66763e531df4ce216
juurailmjarv <juura.ilmjarv@gmail.com>
2018-11-06 16:26:10 +0000

    login check for checkout

--- GuitarShop2/checkout.php
+++ GuitarShop2/checkout.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-
+// Check if the user is logged in, if not then redirect him to index.php page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
 // Include config file
 require_once "config.php";
 // Define variables and initialize with empty values
