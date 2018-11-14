fe8ca0a72e983a0a6609507fdd5e2cecb34e5fe1
juurailmjarv <juura.ilmjarv@gmail.com>
2018-11-14 22:01:13 +0000

    added icon for tab

--- GuitarShop2/header.php
+++ GuitarShop2/header.php
@@ -14,9 +14,11 @@ function cart_item_count()
 <head>
     <meta charset="UTF-8">
     <title>Guitar Shop</title>
+    <link rel="icon" href="./assets/productimages/guitar-black-shape.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/stylesheet.css">
     <link rel="stylesheet" href="./assets/shop-homepage.css">
+
 </head>
 
 <body>
