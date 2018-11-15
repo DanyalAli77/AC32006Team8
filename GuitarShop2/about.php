d1d4c2ccc796684cb98280bb35dbd00bb2613a1b
juurailmjarv <juura.ilmjarv@gmail.com>
2018-11-15 11:16:08 +0000

    Added email contact form

--- GuitarShop2/about.php
+++ GuitarShop2/about.php
@@ -15,14 +15,66 @@ if(!empty($_SESSION['cart'])) {
 
 ?>
 
+    <!-- Page Content -->
     <div class="container">
-        <p>
-            <h1>About page</h1>
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+
+        <!-- Introduction Row -->
+        <h1 class="my-4">About Us
+            <small>It's Nice to Meet You!</small>
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
+
+        <!-- Team Members Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="my-4">Our Team</h2>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+                <h3>John Smith
+                    <small>Job Title</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+                <h3>John Smith
+                    <small>Job Title</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+                <h3>John Smith
+                    <small>Job Title</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+                <h3>John Smith
+                    <small>Job Title</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+                <h3>John Smith
+                    <small>Job Title</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+                <h3>John Smith
+                    <small>Job Title</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+        </div>
+
     </div>
+    <!-- /.container -->
 
 
 
--- GuitarShop2/contact.php
+++ GuitarShop2/contact.php
@@ -10,8 +10,101 @@ else{
     include ('header.php');
 }
 if(!empty($_SESSION['cart'])) {
-    $items =  $_SESSION['cart'];
+    $items = $_SESSION['cart'];
 }
+
+if(isset($_POST['email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "jilmjarv@dundee.ac.uk";
+    $email_subject = "Customer contact form";
+
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+
+
+    // validation expected data exists
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['last_name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['telephone']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+
+
+    $first_name = $_POST['first_name']; // required
+    $last_name = $_POST['last_name']; // required
+    $email_from = $_POST['email']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if(!preg_match($email_exp,$email_from)) {
+        $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if(!preg_match($string_exp,$first_name)) {
+        $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+    }
+
+    if(!preg_match($string_exp,$last_name)) {
+        $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+    }
+
+    if(strlen($comments) < 2) {
+        $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+    }
+
+    if(strlen($error_message) > 0) {
+        died($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+
+    function clean_string($string) {
+        $bad = array("content-type","bcc:","to:","cc:","href");
+        return str_replace($bad,"",$string);
+    }
+
+
+
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+
+// create email headers
+    $headers = 'From: '.$email_from."\r\n".
+        'Reply-To: '.$email_from."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+
+
+    //<!-- include your own success html here -->
+
+    echo "<script type='text/javascript'>
+                alert('Thank you for contacting us. We will be in touch with you very soon.');
+            </script>";
+
+
+
+}
+
+
 
 ?>
     <style>
@@ -24,116 +117,170 @@ if(!empty($_SESSION['cart'])) {
     </style>
 
     <div class="container">
-        <p>
+        <p> </p>
         <h1>Contact us</h1>
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div class="row">
+            <div class="col">
+                <p> </p>
 
 
 
-        <div id="map"></div>
-        <script>
-        function initMap() {
-            var branch1 = {lat: 56.4584978, lng: -2.9838224};
-            var branch2 = {lat: 55.9445158, lng: -3.1914353};
-            var branch3 = {lat: 57.16476,   lng: -2.1037197};
-            var branch4 = {lat: 55.87151,   lng: -4.2909549};
-        var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 7,
-        center: branch1
-        });
 
-        var contentString1 = '<div id="content">'+
-            '<div id="siteNotice">'+
-                '</div>'+
-            '<h3 id="firstHeading" class="firstHeading">Dundee Branch</h3>'+
-            '<div id="bodyContent">'+
-                '<p>Location: 1 High Street, Dundee, DD1 123<br>' +
-            '       Telephone: 01382 123 456<br>' +
-            '       Email: <a href="mailto:Dundee@guitarshop.com" target="_top">Dundee@guitarshop.com</a><br>' +
-            '       Opening hours: ' +
-            '<ul>' +
-            '   <li>Monday: 0900 - 1800</li>' +
-            '   <li>Tuesday: 0900 - 1800</li>' +
-            '   <li>Wednesday: 0900 - 1800</li>' +
-            '   <li>Thursday: 0900 - 1900</li>' +
-            '   <li>Friday: 0900 - 1800</li>' +
-            '   <li>Saturday: 0900 - 1800</li>' +
-            '   <li>Sunday: 1000 - 1600</li>' +
-            '</ul>' +
-            '</p>'+
+                    <div id="map"></div>
+                    <script>
+                    function initMap() {
+                        var branch1 = {lat: 56.4584978, lng: -2.9838224};
+                        var branch2 = {lat: 55.9445158, lng: -3.1914353};
+                        var branch3 = {lat: 57.16476,   lng: -2.1037197};
+                        var branch4 = {lat: 55.87151,   lng: -4.2909549};
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 7,
+                    center: branch1
+                    });
 
-                '</div>'+
-            '</div>';
+                    var contentString1 = '<div id="content">'+
+                        '<div id="siteNotice">'+
+                            '</div>'+
+                        '<h3 id="firstHeading" class="firstHeading">Dundee Branch</h3>'+
+                        '<div id="bodyContent">'+
+                            '<p>Location: 1 High Street, Dundee, DD1 123<br>' +
+                        '       Telephone: 01382 123 456<br>' +
+                        '       Email: <a href="mailto:Dundee@guitarshop.com" target="_top">Dundee@guitarshop.com</a><br>' +
+                        '       Opening hours: ' +
+                        '<ul>' +
+                        '   <li>Monday: 0900 - 1800</li>' +
+                        '   <li>Tuesday: 0900 - 1800</li>' +
+                        '   <li>Wednesday: 0900 - 1800</li>' +
+                        '   <li>Thursday: 0900 - 1900</li>' +
+                        '   <li>Friday: 0900 - 1800</li>' +
+                        '   <li>Saturday: 0900 - 1800</li>' +
+                        '   <li>Sunday: 1000 - 1600</li>' +
+                        '</ul>' +
+                        '</p>'+
 
-
-
-            var contentString2 = '<div id="content">'+
-                '<div id="siteNotice">'+
-                '</div>'+
-                '<h3 id="firstHeading" class="firstHeading">Edinburgh Branch</h3>'+
-                '<div id="bodyContent">'+
-                '<p>Location: 1 Queen Street, Edinburgh, EH8 9YL<br>' +
-                '       Telephone: 0131 123 4567<br>' +
-                '       Email: <a href="mailto:Edinburgh@guitarshop.com" target="_top">Edinburgh@guitarshop.com</a><br>' +
-                '       Opening hours: ' +
-                '<ul>' +
-                '   <li>Monday: 0900 - 1800</li>' +
-                '   <li>Tuesday: 0900 - 1800</li>' +
-                '   <li>Wednesday: 0900 - 1800</li>' +
-                '   <li>Thursday: 0900 - 1900</li>' +
-                '   <li>Friday: 0900 - 1800</li>' +
-                '   <li>Saturday: 0900 - 1800</li>' +
-                '   <li>Sunday: 1000 - 1600</li>' +
-                '</ul>' +
-
-                '</p>'+
-
-
-                '</div>'+
-                '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString1,
-            width: 400,
-            height: 700
-        });
-
-        var infowindow2 = new google.maps.InfoWindow({
-            content: contentString2
-        });
-
-        var marker = new google.maps.Marker({
-        position: branch1,
-        map: map,
-        title: 'Branch1'
-        });
-
-        var marker2 = new google.maps.Marker({
-            position: branch2,
-            map: map,
-            title: 'Branch2'
-        });
-
-        marker.addListener('click', function() {
-        infowindow.open(map, marker);
-        });
-
-        marker2.addListener('click', function() {
-            infowindow2.open(map, marker2);
-        });
-        }
-        </script>
-        <script async defer
-                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJzx5QKDhaB72kgywxbbcXNpEsRH-xH2g&callback=initMap">
-        </script>
+                            '</div>'+
+                        '</div>';
 
 
 
+                        var contentString2 = '<div id="content">'+
+                            '<div id="siteNotice">'+
+                            '</div>'+
+                            '<h3 id="firstHeading" class="firstHeading">Edinburgh Branch</h3>'+
+                            '<div id="bodyContent">'+
+                            '<p>Location: 1 Queen Street, Edinburgh, EH8 9YL<br>' +
+                            '       Telephone: 0131 123 4567<br>' +
+                            '       Email: <a href="mailto:Edinburgh@guitarshop.com" target="_top">Edinburgh@guitarshop.com</a><br>' +
+                            '       Opening hours: ' +
+                            '<ul>' +
+                            '   <li>Monday: 0900 - 1800</li>' +
+                            '   <li>Tuesday: 0900 - 1800</li>' +
+                            '   <li>Wednesday: 0900 - 1800</li>' +
+                            '   <li>Thursday: 0900 - 1900</li>' +
+                            '   <li>Friday: 0900 - 1800</li>' +
+                            '   <li>Saturday: 0900 - 1800</li>' +
+                            '   <li>Sunday: 1000 - 1600</li>' +
+                            '</ul>' +
+
+                            '</p>'+
+
+
+                            '</div>'+
+                            '</div>';
+
+                    var infowindow = new google.maps.InfoWindow({
+                        content: contentString1,
+                        width: 400,
+                        height: 700
+                    });
+
+                    var infowindow2 = new google.maps.InfoWindow({
+                        content: contentString2
+                    });
+
+                    var marker = new google.maps.Marker({
+                    position: branch1,
+                    map: map,
+                    title: 'Branch1'
+                    });
+
+                    var marker2 = new google.maps.Marker({
+                        position: branch2,
+                        map: map,
+                        title: 'Branch2'
+                    });
+
+                    marker.addListener('click', function() {
+                    infowindow.open(map, marker);
+                    });
+
+                    marker2.addListener('click', function() {
+                        infowindow2.open(map, marker2);
+                    });
+                    }
+                    </script>
+                    <script async defer
+                            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJzx5QKDhaB72kgywxbbcXNpEsRH-xH2g&callback=initMap">
+                    </script>
+                <p></p>
+            </div>
+                <div class="col">
+                    <p> </p>
+                        <form name="contactform" method="post" action="contact.php">
+                            <table width="450px">
+                                <tr>
+                                    <td valign="top">
+                                        <label for="first_name">First Name *</label>
+                                    </td>
+                                    <td valign="top">
+                                        <input  class="form-control" type="text" name="first_name" maxlength="50" size="30">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign="top"">
+                                    <label for="last_name">Last Name *</label>
+                                    </td>
+                                    <td valign="top">
+                                        <input  class="form-control" type="text" name="last_name" maxlength="50" size="30">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign="top">
+                                        <label for="email">Email Address *</label>
+                                    </td>
+                                    <td valign="top">
+                                        <input  class="form-control" type="text" name="email" maxlength="80" size="30">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign="top">
+                                        <label for="telephone">Telephone Number</label>
+                                    </td>
+                                    <td valign="top">
+                                        <input  class="form-control" type="text" name="telephone" maxlength="30" size="30">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign="top">
+                                        <label for="comments">Comments *</label>
+                                    </td>
+                                    <td valign="top">
+                                        <textarea class="form-control"  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" style="text-align:right">
+                                        <p></p>
+                                        <input class="btn btn-primary" type="submit" value="Submit">
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                </div>
+        </div>
     </div>
 
 <?php
 include ('footer.php');
 ?>
+
