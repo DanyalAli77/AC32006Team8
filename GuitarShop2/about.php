ae9c0c62f7661c655774be937d4076c74335760c
Matt Foulis <MatthewFoulis1@gmail.com>
2018-11-15 17:43:28 +0000

    Updated about page

--- GuitarShop2/about.php
+++ GuitarShop2/about.php
@@ -22,7 +22,7 @@ if(!empty($_SESSION['cart'])) {
         <h1 class="my-4">About Us
             <small>It's Nice to Meet You!</small>
         </h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
+        <p>GuitarShop is a specialist music shop specialising in the sale of guitars and related accessories. We are a Scottish based company with several branches in several cities. To see a list of branches, or if you want to get in touch then <a href="contact.php">click here.</a></p>
 
         <!-- Team Members Row -->
         <div class="row">
@@ -30,46 +30,36 @@ if(!empty($_SESSION['cart'])) {
                 <h2 class="my-4">Our Team</h2>
             </div>
             <div class="col-lg-4 col-sm-6 text-center mb-4">
-                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                <img class="rounded-circle img-fluid d-block mx-auto" src="assets/profile2.png" alt="">
+                <!-- image source: https://pixabay.com/en/lady-teacher-profile-head-female-31217/ -->
+                <h4>Judy Smith <small>CEO</small> </h4>
+                <p>Brief description of employee</p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center mb-4">
-                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                <img class="rounded-circle img-fluid d-block mx-auto" src="assets/profile2.png" alt="">
+                <h4>Alice McDonald <small>Head of e-Commerce</small> </h4>
+                <p>Brief description of employee</p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center mb-4">
-                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                <img class="rounded-circle img-fluid d-block mx-auto" src="assets/profile2.png" alt="">
+                <h4>Fiona Black <small>Branch Manager</small> </h4>
+                <p>Brief description of employee</p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center mb-4">
-                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                <img class="rounded-circle img-fluid d-block mx-auto" src="assets/profile.png" alt="">
+                <!-- image source: https://pixabay.com/en/man-avator-person-admin-161282/ -->
+                <h4>John Smith <small>Branch Manager</small> </h4>
+                <p>Brief description of employee</p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center mb-4">
-                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                <img class="rounded-circle img-fluid d-block mx-auto" src="assets/profile.png" alt="">
+                <h4>Harry White <small>Sales Assistant</small> </h4>
+                <p>Brief description of employee</p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center mb-4">
-                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
-                <h3>John Smith
-                    <small>Job Title</small>
-                </h3>
-                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                <img class="rounded-circle img-fluid d-block mx-auto" src="assets/profile.png" alt="">
+                <h4>Cameron McGill <small></small> </h4>
+                <p>Brief description of employee</p>
             </div>
         </div>
 
