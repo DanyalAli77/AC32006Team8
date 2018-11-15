9aa2bfea28bd115801a9f42e417e0e44ac9ace41
Matt Foulis <MatthewFoulis1@gmail.com>
2018-11-15 17:49:04 +0000

    Add error check for cart size

--- GuitarShop2/addtocart.php
+++ GuitarShop2/addtocart.php
@@ -5,7 +5,17 @@ if (empty($_SESSION['cart'])) {
 	    $_SESSION['cart'] = array();
 }
 //put product id in the array
-array_push($_SESSION['cart'], $_GET['id']);
+if(count($_SESSION['cart']) == 10)
+{
+    //Error prevention to stop customer filling cart over the maximum size
+    //Given more time this would be a pop-up on the website.
+    echo "Maximum cart size is 10";
+}
+else
+{
+    array_push($_SESSION['cart'], $_GET['id']);
+}
+
 if(isset($_SESSION['loggedin']))
 {
     header('location: welcome.php?status=success');
